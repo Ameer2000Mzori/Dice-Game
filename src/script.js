d7c2319b7e80f7b9325093ea07b2e583d417b5e5
Ameer <ameerapex496@gmail.com>
2023-12-28 04:22:21 +0100

    added comments and selected elements needed for the creation of game

--- src/script.js
+++ src/script.js
@@ -1,7 +1,27 @@
 // selecting elements
+//
+////buttons selecte
 const restartBtn = document.getElementsByClassName("restart-Btn")[0];
 const rollDiceBtn = document.getElementsByClassName("roll-Dice-Btn")[0];
 const holdBtn = document.getElementsByClassName("hold-Btn")[0];
+//
+//player one selected
+const playerLeft = document.getElementsByClassName("player-Left")[0];
+const player1PointCurrent = document.getElementsByClassName(
+  "player-1-Point-Current"
+)[0];
+const player1PointCount = document.getElementsByClassName(
+  "player-1-Point-Count"
+);
+//
+//player two selected
+const playerRight = document.getElementsByClassName("player-Right")[0];
+const player2PointCurrent = document.getElementsByClassName(
+  "player-2-Point-Current"
+)[0];
+const player2PointCount = document.getElementsByClassName(
+  "player-2-Point-Count"
+);
 
 // gelobal varibales
 
