f863ba3c69e6225f8a8d0d75371bb8411b28fe2d
Ameer <ameerapex496@gmail.com>
2023-12-28 04:36:21 +0100

    added random image change when dice is rolled + random number is created

--- src/script.js
+++ src/script.js
@@ -1,10 +1,9 @@
 // selecting elements
-//
+const diceImgEl = document.getElementsByClassName("dice-Img")[0];
 ////buttons selecte
 const restartBtn = document.getElementsByClassName("restart-Btn")[0];
 const rollDiceBtn = document.getElementsByClassName("roll-Dice-Btn")[0];
 const holdBtn = document.getElementsByClassName("hold-Btn")[0];
-//
 //player one selected
 const playerLeft = document.getElementsByClassName("player-Left")[0];
 const player1PointCurrent = document.getElementsByClassName(
@@ -13,7 +12,6 @@ const player1PointCurrent = document.getElementsByClassName(
 const player1PointCount = document.getElementsByClassName(
   "player-1-Point-Count"
 );
-//
 //player two selected
 const playerRight = document.getElementsByClassName("player-Right")[0];
 const player2PointCurrent = document.getElementsByClassName(
@@ -30,6 +28,9 @@ let playerTwo = false;
 // functions
 const rollDice = () => {
   // create random number between 1-6
+  let randNum = Math.round(Math.random() * 5) + 1;
+  console.log(randNum);
+  diceImgEl.style.backgroundImage = `url(./assets/dice-${randNum}.png)`;
   ////show that number to the screen
   //////the active player should have that number in current
 };
