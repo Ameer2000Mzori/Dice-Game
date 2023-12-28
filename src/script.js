de56f2f8ef701006103a8e693789a702fd5aa70b
Ameer <ameerapex496@gmail.com>
2023-12-28 04:19:53 +0100

    added and selected elements needed for coding / added comment to js file

---
+++ src/script.js
@@ -0,0 +1,10 @@
+// selecting elements
+const restartBtn = document.getElementsByClassName("restart-Btn")[0];
+const rollDiceBtn = document.getElementsByClassName("roll-Dice-Btn")[0];
+const holdBtn = document.getElementsByClassName("hold-Btn")[0];
+
+// gelobal varibales
+
+// functions
+
+// event listners
